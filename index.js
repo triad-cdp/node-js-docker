13175dd62a8ea601a362ebd0647bff2e7a5ee85c
CVJT <chris@hsot.co>
2016-11-28 16:33:54 +0000

    Test Demo test commit to trigger SNAP-CI pipeline.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World! - Demo');
+  response.send('Hello World! - Demo for chris');
 });
 
 app.listen(app.get('port'), function() {
